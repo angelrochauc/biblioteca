bec621860b920122579d49745f7aaef1a5250a83
Angel Junior Rocha Guerrero <75550027+angelrochauc@users.noreply.github.com>
2023-02-02 17:03:22 -0500

    first commit

---
+++ Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Aqui se realiza el llamado para usar la base de datos
+    const BASE_URL = "http://localhost/Biblioteca/";
+    const HOST = "localhost";
+    const BD = "bibliotecaAngel";
+    const DB_USER = "root";
+    const PASS = "";
+    const CHARSET = "charset=utf8";
+?>
